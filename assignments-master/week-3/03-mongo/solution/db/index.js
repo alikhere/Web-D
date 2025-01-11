33394545ca24556f6dcde527a6953f002f47a560
alikhere <alikhurshidhere@gmail.com>
2025-01-12 04:46:39 +0530

    coursePurchaseApp_added

--- assignments-master/week-3/03-mongo/solution/db/index.js
+++ assignments-master/week-3/03-mongo/solution/db/index.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://kirags123:8qPEa8KTKBEh2bss@cluster0.f3qlbuo.mongodb.net/course_selling_app');
+mongoose.connect('mongodb+srv://alikhere:Ali8540005317@cluster0.josue.mongodb.net/course_selling_app');
+//mongoose.connect('mongodb+srv://kirags123:8qPEa8KTKBEh2bss@cluster0.f3qlbuo.mongodb.net/course_selling_app');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
